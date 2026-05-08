385f0200f69eaaea72da3125f763c64263ac343f
asikrshoudo <shoudo@proton.me>
2026-05-09 02:25:07 +0600

    fix: correct full name spelling

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 // ? '\n' are important for the formatting of the output, so don't remove them. - @Ruhannn
 const config = {
-  name: "shoudo",
+  name: "Ashikur Sheikh Shoudo",
   bio: "\n Ex-SPSCian • CEO of TheKami • CTO of SaltDrio \n",
   about: `Hey, I am shoudo. I love traveling — skydiving is on my bucket list.
 In my free time, I watch anime & movies, follow UFC & F1, and play Minecraft.
